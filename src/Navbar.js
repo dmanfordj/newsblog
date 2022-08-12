4497dac25648f3bf9ff4f163bd7f3d810bdf6958
Danny <dmanfordj@gmail.com>
2022-08-12 01:22:14 +0100

    hj

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
         <div className='navbar--css'>
         <nav class="navbar bg-light">
     <div class="container-fluid">
-    <a class="navbar-brand" href="www.123.com">Navbar</a>
+    <a class="navbar-brand" href="www.123.com.gh">Navbar</a>
     <form class="d-flex" role="search">
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
       <button class="btn btn-outline-success" type="submit">Search</button>
